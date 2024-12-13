3aaa55e757ba1fc0fbf9aaa6d900b9b1298d3723
safadigital <safarov@safadigital.su>
2024-12-13 20:58:43 +0500

    добавил стилей

--- components/AdminInfo/AdminInfo.tsx
+++ components/AdminInfo/AdminInfo.tsx
@@ -2,8 +2,11 @@ import styles from './AdminInfo.module.css'
 import Image from 'next/image';
 import avatar from '../../public/user_avatar.png';
 import EditIcon from './edit.png';
+import DeleteIcon from './delete.png';
+import rectAdminImg from './rectadmin.png';
 import Link from 'next/link';
 import { AdminInfoProps } from './AdminInfo.props';
+
 
 
 export const AdminInfo = ({ className, ...props }: AdminInfoProps): JSX.Element => {
@@ -11,11 +14,19 @@ export const AdminInfo = ({ className, ...props }: AdminInfoProps): JSX.Element 
     return (
         <>
         <div className={styles.adminInfo} {...props}>
+            <div className={styles.userPhoto}>
+
+          
         <div className={styles.profilePic}>
         <div className={styles.avatar}><Image src={avatar} alt="avatar" /></div>
         <Link href="/admin/uploadphoto" className={styles.link}>
 				<Image className={styles.edit} src={EditIcon} alt='Edit profile' title='Загрузить фото' />
 			</Link>
+        </div>
+        <div className={styles.rectangle}>
+            <Image src={rectAdminImg} alt='rectangle'/>
+            <p className={styles.userRole}>Super Admin</p>
+        </div>
         </div>
 
         <section className={styles.infos}>
@@ -54,25 +65,38 @@ export const AdminInfo = ({ className, ...props }: AdminInfoProps): JSX.Element 
 <button>Изменить</button>
 </div>
 
-<div className={styles.delete}>
-<span>Удалить профиль</span>
-</div>
+
             
         </div>
 
-       
-
+     
           
 
             </div>
+           
         </section>
 
-        <div className={styles.userCard}>
+      
+{/* 
+        <div className={styles.delete}>
+<Image src={DeleteIcon} alt='Edit profile' />
+<span>Удалить профиль</span>
+</div> */}
 
-        </div>
+        {/* <div className={styles.userCard}>
 
-        </div>
+        </div> */}
+
+
+  {/* <div></div> */}
+
        
+
+        </div>
+        <div className={styles.delete}>
+<Image src={DeleteIcon} alt='Edit profile' />
+<span>Удалить профиль</span>
+</div>
         </>
     )
 }
