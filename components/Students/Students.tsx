2814c21ecc0c1f2a522195d1b36e2174aab358e7
safakun <safarovdeem@gmail.com>
2024-12-14 13:25:56 +0500

    сделал стили компонента ученики

--- components/Students/Students.tsx
+++ components/Students/Students.tsx
@@ -1,30 +1,46 @@
-import styles from './Students.module.css'
+import styles from './Students.module.css';
+import avatar from '../../public/user_avatar.png';
+import Image from 'next/image';
+import DotsIcon from './dots_icon.png';
 
 export const Students = () => {
 
     return (
+        <>
         <table className={styles.students}>
   <tr>
+    <th></th>
     <th>Ученик</th>
     <th>Должность</th>
     <th>Номер телефона</th>
     <th>E-mail</th>
     <th>Назначенные проекты</th>
+    <th></th>
   </tr>
   <tr>
-    <td>Александр Изотов</td>
+    <td className={styles.userImage}><Image src={avatar} alt='avatar' /> </td>
+    <td> Александр Изотов</td>
     <td>Электрик</td>
     <td>+37529788888</td>
     <td>izotov@gmail.com</td>
     <td>Проект 1, Мой проект</td>
+    <td><Image className={styles.addStudent} src={DotsIcon} alt='add student' /></td>
   </tr>
   <tr>
+  <td className={styles.userImage}><Image src={avatar} alt='avatar' /> </td>
     <td>Антон Осипов</td>
     <td>Электрик</td>
     <td>+37529788888</td>
     <td>osipov@gmail.com</td>
     <td>Мой проект</td>
+    <td><Image src={DotsIcon} alt='add student' /></td>
   </tr>
 </table>
+
+
+<button className={styles.addStudentButton}>
+    Добавить ученика
+</button>
+</>
     )
 }
