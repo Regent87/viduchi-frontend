5323c7cc9462713cff1adfa49c1588a6ae688e51
safakun <safarovdeem@gmail.com>
2024-12-25 20:22:24 +0500

    fixed styles

--- app/api.ts
+++ app/api.ts
@@ -1,16 +1,16 @@
 export const API = {
 	auth: {
-		login: process.env.NEXT_PUBLIC_API_URL + '/v1/admin-api/auth/login',
-		confirm: process.env.NEXT_PUBLIC_API_URL + '/v1/admin-api/auth/confirm',
-		me: process.env.NEXT_PUBLIC_API_URL + '/v1/admin-api/auth/me',
-		logout: process.env.NEXT_PUBLIC_API_URL + '/v1/admin-api/auth/logout',
+		login: process.env.NEXT_PUBLIC_API_URL + '/v1/auth/login',
+		confirm: process.env.NEXT_PUBLIC_API_URL + '/v1/auth/confirm',
+		me: process.env.NEXT_PUBLIC_API_URL + '/v1/auth/me',
+		logout: process.env.NEXT_PUBLIC_API_URL + '/v1/auth/logout',
 	},
 	projects: {
-		create: process.env.NEXT_PUBLIC_API_URL + '/v1/admin-api/projects',
-		list: process.env.NEXT_PUBLIC_API_URL + '/v1/admin-api/projects',
-		byId: (id: number) => process.env.NEXT_PUBLIC_API_URL + `/v1/admin-api/projects/${id}`,
+		create: process.env.NEXT_PUBLIC_API_URL + '/v1/projects',
+		list: process.env.NEXT_PUBLIC_API_URL + '/v1/projects',
+		byId: (id: number) => process.env.NEXT_PUBLIC_API_URL + `/v1/projects/${id}`,
 	},
 	students: {
-		create: process.env.NEXT_PUBLIC_API_URL + '/v1/admin-api/students'
+		create: process.env.NEXT_PUBLIC_API_URL + '/v1/students'
 	}
 };
