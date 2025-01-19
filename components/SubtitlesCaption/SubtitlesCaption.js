f38c9ddf0814d106d5b0f97c500e7f2be977deff
safadigital <safarov@safadigital.su>
2025-01-19 10:02:39 +0500

    fixed styles subtitles

--- components/SubtitlesCaption/SubtitlesCaption.js
+++ components/SubtitlesCaption/SubtitlesCaption.js
@@ -12,13 +12,15 @@ import {
   
     const styleCombined = {
       fontFamily: 'Arial',
-      fontSize: '11vmin',
+      fontSize: '7vmin',
       textAlign: 'center',
       fontWeight: 'bold',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
       color: 'white',
       textShadow: "-1px 1px 10px rgba(1, 1, 1, 0.95)",
       position: "absolute",
-      bottom: "20px",
+      bottom: "50px",
+      marginBottom: '10px',
       left: "50%",
       transform: "translateX(-50%)",
       opacity,
