51ea27d05588dd5bf9c3a9b6b83f9512d8c206c3
safadigital <safarov@safadigital.su>
2025-03-01 19:42:47 +0500

    added render audio at server

--- server.mjs
+++ server.mjs
@@ -129,6 +129,77 @@ return res.status(200).sendFile(fileName, options, function (err) {
 
 
 
+// render audio
+app.get('/api/renderaudio', async (req, res) => {
+
+  // The composition you want to render
+  const compositionId = 'myComp';
+  
+  console.log("PAth: ", path.join(process.cwd(), './components/player/root.ts'))
+   
+  // You only have to create a bundle once, and you may reuse it
+  // for multiple renders that you can parametrize using input props.
+  const bundleLocation = await bundle({
+   ignoreRegisterRootWarning: true,
+    entryPoint: path.join(process.cwd(), './components/player/root.ts'),
+    // If you have a webpack override in remotion.config.ts, pass it here as well.
+    // webpackOverride: (config) => config,
+  });
+  
+  
+  console.log(" Budnle location: ", bundleLocation);
+  
+  const compositionsList = await getCompositions(bundleLocation);
+  
+  console.log("Compositions list: ", compositionsList)
+   
+  // Parametrize the video by passing props to your component.
+  const inputProps = {
+    // foo: 'bar',
+  };
+   
+  // Get the composition you want to render. Pass `inputProps` if you
+  // want to customize the duration or other metadata.
+  const composition = await selectComposition({
+    serveUrl: bundleLocation,
+    id: compositionId,
+    inputProps,
+  });
+  
+  console.log("Composition: ", composition)
+   
+  // Render the video. Pass the same `inputProps` again
+  // if your video is parametrized with data.
+  
+  const finalOutput = `out/${compositionId}.mp3`;
+  
+  await renderMedia({
+    composition,
+    serveUrl: bundleLocation,
+    codec: 'h264',
+    outputLocation: finalOutput,
+    inputProps,
+  });
+  
+  console.log('Render audio done!');
+  
+  const options = {
+    root:  path.join(process.cwd(), './out/')
+  };
+  
+  const fileName =`${compositionId}.mp3`;
+  return res.status(200).sendFile(fileName, options, function (err) {
+    if (err) {
+        console.error('Error sending file:', err);
+    } else {
+        console.log('Sent:', fileName);
+    }
+  });
+  
+  
+  })
+
+
 // GET запрос из компонента - отправляем ему данные из файла json в формате json
 
 // app.get("/", async (req: any, res: any) => {
