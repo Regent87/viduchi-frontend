b0e4db06cd4914fb92598633425daf68d8ea004e
safadigital <safarov@safadigital.su>
2025-03-02 14:41:47 +0500

    added component while new instruction is generated

---
+++ components/InstructionNewCreationCard/InstructionNewCreationCard.tsx
@@ -0,0 +1,59 @@
+
+import styles from './InstructionNewCreationCard.module.css';
+import cn from 'classnames';
+import MenuIcon from './menu.svg';
+import PlayIcon from './play.svg';
+import cover from '../../public/cover.png';
+import Image from 'next/image';
+import { useState, useEffect, useRef } from 'react';
+
+
+export const InstructionNewCreationCard = () => {
+
+	const [isEditOpen, setIsEditOpen] = useState(false);
+	const closeDropdown = () => {
+		setIsEditOpen(false);
+	};
+
+	const menuRef = useRef<HTMLDivElement>(null);
+
+	// useEffect(() => {
+	// 	const handleClickOutside = (event: MouseEvent) => {
+	// 		if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+	// 			setIsEditOpen(false);
+	// 		}
+	// 	};
+
+	// 	document.addEventListener('mousedown', handleClickOutside);
+	// 	return () => {
+	// 		document.removeEventListener('mousedown', handleClickOutside);
+	// 	};
+	// }, []);
+
+	return (
+		<>
+			<div className={cn(styles.instruction)}>
+				<div className={styles.instructionCover}>
+					{/* <Image src={instructionModel.cover_url ? instructionModel.cover_url : cover } alt="cover" width={220} height={150} /> */}
+					{/* <PlayIcon /> */}
+				</div>
+				<div className={styles.dateAndTitle}>
+					<div className={styles.instructionTitle}>
+						{/* { instructionModel.title } */}
+                        Инструкция создается...
+					</div>
+					<div className={styles.menuContainer} ref={menuRef}>
+						{/* <MenuIcon className={styles.menu} onClick={() => setIsEditOpen(!isEditOpen)} /> */}
+						{/* {
+							isEditOpen && (
+								<EditInstructionMenu closeDropdown={closeDropdown}
+								instruction={instructionModel}
+								/>
+							)
+						} */}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
